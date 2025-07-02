2b4bc3018f4ce0d4b485afcf742db37c7ee3f1bb
oonamo <onam75hernandez@gmail.com>
2025-07-02 16:03:11 -0700

    add image scaling to the cookie

--- src/components/cookie.jsx
+++ src/components/cookie.jsx
@@ -12,24 +12,40 @@ export const Events = Object.freeze({
 export function Cookie({ scaling, multiplier, eventHandle }) {
   const [clicks, setClicks] = useState(0)
   const [maxClicks, setMaxClicks] = useState(CONST.STARTING_CLICKS)
+  const [scale, setScale] = useState(1);
+  const [cookiesDestroyed, setCookiesDestroyed] = useState(0);
+
+
+  function transformImage() {
+    const image = document.querySelector(".cookie")
+    image.style.transform = `scale(${scale}, ${scale})`
+  }
 
   function onClick() {
     setClicks(1 + multiplier(clicks))
     eventHandle(Events.CLICKED, clicks, maxClicks)
 
+    setScale(scale - 0.6 / maxClicks)
+    console.log("scale: ", scale)
+    console.log("steps: ", 0.6 / 10)
+
     if (maxClicks - clicks <= 1) {
       eventHandle(Events.DESTROYED, clicks, maxClicks)
-      setClicks(0);
-      setMaxClicks(Math.floor(scaling(maxClicks)));
+      setClicks(0)
+
+      setMaxClicks(Math.floor(scaling(maxClicks)))
+      setCookiesDestroyed(cookiesDestroyed + 1)
+
+      setScale(1 + cookiesDestroyed * 0.05)
     }
+
+    transformImage()
   }
 
 
   return (
     <div className="main-cookie">
-      {/* Should be an image */}
       <img src={CookiePicture} onClick={onClick} className="cookie" />
-      {/* <button onClick={onClick} className="cookie-btn">Cookie</button> */}
       <p className="clicks-remaining">{maxClicks - clicks}</p>
     </div>
   )
